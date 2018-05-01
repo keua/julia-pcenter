4e70f8e2e37c0b75d44fae4c36da9b7bdd570776
keua <k3n3th@gmail.com>
2018-05-01 19:17:10 +0200

    hard script fix

--- runnable/run-hard.sh
+++ runnable/run-hard.sh
@@ -2,12 +2,13 @@
 JULIA_HOME=$1
 INST_HOME=$2
 method=$3
-limit=$4
-for ((i=1;i<$limit+1;i++))
+start=$4
+end=$5
+for ((i=$start;i<$end+1;i++))
 {
     id=$i"hard"
     filename="instance"$i".dat"
     echo "Running file: "$filename
-    $JULIA_HOME/julia pcenter_solver.jl -f="$INST_HOME/$filename" -m=$method -l=1 | tee logs/$method"-"$id.log
+    $JULIA_HOME/julia ../pcenter_solver.jl -f="$INST_HOME/$filename" -m=$method -l=1 | tee logs/$method"-"$id.log
 }
 exit 0
